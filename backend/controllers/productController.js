75436662d8376d0831ad5de40054e13300ce3c94
Sohum Khot <skhot@zmags.com>
2023-05-31 19:43:53 +0530

    feat: Added Pagination support for Product API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,7 +14,9 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter();
+  const resultPerPage = 5;
+
+  const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
   const products = await apiFeature.query;
 
   res.status(201).json({
