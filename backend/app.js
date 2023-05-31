cb482064aa9a51c0870e7f235cba6b0390dce44b
Sohum Khot <skhot@zmags.com>
2023-05-31 11:44:22 +0530

    refactor: Added ErrorHandler class

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
+const errorMiddleWare = require("./middleware/error");
 
 const app = express();
 
 // Route Imports
-const products = require('./routes/productRoute');
+const products = require("./routes/productRoute");
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json()); 
-app.use('/api/v1', products);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api/v1", products);
 
+// Middleware for Errors
+app.use(errorMiddleWare);
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
 
 // Create Product -- Admin
 exports.createProduct = async (req, res, next) => {
@@ -23,17 +24,14 @@ exports.getProductDetails = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
 
   res.status(200).json({
     success: true,
-    product
+    product,
   });
-}
+};
 
 // Update Product -- Admin
 exports.updateProduct = async (req, res) => {
