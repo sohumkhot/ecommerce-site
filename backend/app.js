dbeab61856c82aa47d6eb54bf3f46b26dab59b69
Sohum Khot <skhot@zmags.com>
2023-06-01 16:22:18 +0530

    feat: Added User Routes

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,13 @@ const app = express();
 
 // Route Imports
 const products = require('./routes/productRoute');
+const user = require('./routes/userRoute');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use('/api/v1', products);
+app.use('/api/v1', user);
 
 // Middleware for Errors
 app.use(errorMiddleWare);
---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const User = require('../models/userModel');
+
+// Register a User
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: { public_id: 'this is a sample id', url: 'profile sample Url' },
+  });
+
+  res.status(201).json({
+    success: true,
+    user,
+  });
+});
