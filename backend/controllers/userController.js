fdca18efbc845532f41c7dcea277b99e650cbecc
Sohum Khot <skhot@zmags.com>
2023-06-01 17:47:39 +0530

    feat: Added Login functionality with JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const User = require('../models/userModel');
+const sendToken = require('../utils/jwtToken');
 
 // Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -13,8 +14,29 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     avatar: { public_id: 'this is a sample id', url: 'profile sample Url' },
   });
 
-  res.status(201).json({
-    success: true,
-    user,
-  });
+  sendToken(user, 201, res);
+});
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Checking if user has given password and email both
+  if (!email || !password) {
+    return next(new ErrorHandler('Please Enter Email & Password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorHandler('Invalid email or password', 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler('Invalid email or password', 401));
+  }
+
+  sendToken(user, 200, res);
 });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,4 +40,28 @@ const userSchema = new mongoose.Schema({
   resetPasswordExpire: Date,
 });
 
+userSchema.pre('save', async function (next) {
+  // IF password was not modified (profile pic or anything is updated)
+  // Dont rehash the password because its already hashed
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  // ELSE hash the password because its updated
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// JWT TOKEN
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// Compare Password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model('User', userSchema);
+ 
