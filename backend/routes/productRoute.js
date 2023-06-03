2ce1fec4dd5b6a7e50036a918dffe9e883f623fb
Sohum Khot <skhot@zmags.com>
2023-06-03 12:57:59 +0530

    feat: Added role authorizer, user field in product schema

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,16 +6,20 @@ const {
   deleteProduct,
   getProductDetails,
 } = require('../controllers/productController');
-const { isAuthenticatedUser } = require('../middleware/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route('/products').get(isAuthenticatedUser, getAllProducts);
-router.route('/product/new').post(createProduct);
+router.route('/products').get(getAllProducts);
+
+router
+  .route('/product/new')
+  .post(isAuthenticatedUser, authorizeRoles('admin'), createProduct);
+
 router
   .route('/product/:id')
-  .put(updateProduct)
-  .delete(deleteProduct)
+  .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct)
   .get(getProductDetails);
 
 module.exports = router;
